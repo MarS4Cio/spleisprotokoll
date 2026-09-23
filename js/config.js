98ccf399875785b11ac08e795949109631b93ad3
MarS4Cio <elstein.florian@gmail.com>
2026-09-23 15:52:55 +0200

    Add application configuration in config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const APP_CONFIG = {
+  companyName: "Geothermie Unterhaching GmbH & Co. KG",
+  shortName: "Geothermie Unterhaching",
+  companySubtext: "Technik GUH",
+  logoPath: "./GUH_Logo.png",
+  recipients: [
+    "technik@geothermie-unterhaching.de",
+    "f.elstein@geothermie-unterhaching.de",
+    "Netzausbau@geothermie-unterhaching.de"
+  ],
+  // Hier deine Client-ID aus der Registrierung im Microsoft Entra Admin Center eintragen:
+  msalClientId: "DEINE_MICROSOFT_CLIENT_ID_HIER_EINFUEGEN"
+};
